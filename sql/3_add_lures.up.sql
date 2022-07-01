82b43aec9a18d2a9c4c41e4f47943c1728494e12
James Berry <james@coppermoth.com>
2022-07-01 13:21:34 +0100

    Fix up upgrade for lures

--- sql/3_add_lures.up.sql
+++ sql/3_add_lures.up.sql
@@ -1,2 +1,5 @@
 alter table pokemon
     modify seen_type enum ('wild', 'encounter', 'nearby_stop', 'nearby_cell', 'lure_wild', 'lure_encounter') null;
+
+alter table pokemon_history
+    modify seen_type enum ('wild', 'encounter', 'nearby_stop', 'nearby_cell', 'lure_wild', 'lure_encounter') null;
