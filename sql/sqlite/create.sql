2f384513de0936e6a6725c2c05436f4f258260ad
Fabio1988 <35898099+Fabio1988@users.noreply.github.com>
2023-03-09 14:33:57 +0100

    remove default

--- sql/sqlite/create.sql
+++ sql/sqlite/create.sql
@@ -23,7 +23,7 @@ CREATE TABLE pokemon (
                          costume smallint  ,
                          first_seen_timestamp int  NOT NULL,
                          changed int   DEFAULT 0 NOT NULL,
-                         `iv` float(5,2) unsigned DEFAULT NULL,
+                         `iv` float(5,2) unsigned,
                          cell_id bigint  ,
                          expire_timestamp_verified TINYINT NOT NULL,
                          display_pokemon_id smallint  ,
