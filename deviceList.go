a7affe1e07bd9ee0b5f38961e7225f3e9b37bd1c
James Berry <james@coppermoth.com>
2023-06-11 14:30:22 +0100

    Add last update

--- deviceList.go
+++ deviceList.go
@@ -1,18 +1,23 @@
 package main
 
-import "github.com/puzpuzpuz/xsync/v2"
+import (
+	"github.com/puzpuzpuz/xsync/v2"
+	"time"
+)
 
 type DeviceLocation struct {
-	Latitude  float64
-	Longitude float64
+	Latitude   float64
+	Longitude  float64
+	LastUpdate int64
 }
 
 var deviceLocation = xsync.NewMapOf[DeviceLocation]()
 
 func UpdateDeviceLocation(deviceId string, lat, lon float64) {
 	deviceLocation.Store(deviceId, DeviceLocation{
-		Latitude:  lat,
-		Longitude: lon,
+		Latitude:   lat,
+		Longitude:  lon,
+		LastUpdate: time.Now().Unix(),
 	})
 }
 
