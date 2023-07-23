563b0fb9de12c9c0320138433994ef24b1a3b613
Fabio1988 <35898099+Fabio1988@users.noreply.github.com>
2023-07-23 21:26:18 +0200

    Update routes.go

--- routes.go
+++ routes.go
@@ -75,7 +75,7 @@ func Raw(c *gin.Context) {
 	pogodroidHeader := r.Header.Get("origin")
 	userAgent := r.Header.Get("User-Agent")
 
-	log.Infof("Raw: Received data from %s", body)
+	//log.Infof("Raw: Received data from %s", body)
 	//log.Infof("User agent is %s", userAgent)
 
 	if pogodroidHeader != "" {
