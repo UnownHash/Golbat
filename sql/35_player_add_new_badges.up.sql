18d0d591fc6be8aa18e8e85b1353487dfa1c9059
Fabio1988 <fabio.schett@gmail.com>
2025-01-14 15:08:15 +0100

    feat: added new badges to player

---
+++ sql/35_player_add_new_badges.up.sql
@@ -0,0 +1,5 @@
+alter table `player`
+    add `dex_gen10`         TINYINT UNSIGNED DEFAULT NULL AFTER `dex_gen9`,
+    add `event_check_ins`   INT(6) UNSIGNED DEFAULT NULL AFTER `showcase_max_size_first_place`,
+    add `parties_completed` INT(6) UNSIGNED DEFAULT NULL AFTER `showcase_max_size_first_place`,
+    add `total_route_play`  INT(6) UNSIGNED  DEFAULT NULL AFTER `showcase_max_size_first_place`;
