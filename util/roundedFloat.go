b138bee72e281caccf534684cb97caf103f30da4
James Berry <james@coppermoth.com>
2025-04-07 10:32:19 +0100

    switch to 64 bitsize

--- util/roundedFloat.go
+++ util/roundedFloat.go
@@ -5,5 +5,5 @@ import "strconv"
 type RoundedFloat4 float64
 
 func (r RoundedFloat4) MarshalJSON() ([]byte, error) {
-	return []byte(strconv.FormatFloat(float64(r), 'f', 4, 32)), nil
+	return []byte(strconv.FormatFloat(float64(r), 'f', 4, 64)), nil
 }
