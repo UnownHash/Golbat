c58433ca687c5d8d19c70f05eda5496ffc083f4a
Fabio1988 <fabio.schett@gmail.com>
2025-04-30 11:09:31 +0200

    fix: revert that change in this migration
    * not related, leftover / typo

--- sql/3_add_lures.up.sql
+++ sql/3_add_lures.up.sql
@@ -1,5 +1,5 @@
 alter table pokemon
-    modify seen_type enum ('wild', 'encounter', 'nearby_stop', 'nearby_cell', 'lure_wild', 'lure_encounter', 'tappable_encounter') null;
+    modify seen_type enum ('wild', 'encounter', 'nearby_stop', 'nearby_cell', 'lure_wild', 'lure_encounter') null;
 
 alter table pokemon_history
-    modify seen_type enum ('wild', 'encounter', 'nearby_stop', 'nearby_cell', 'lure_wild', 'lure_encounter', 'tappable_encounter') null;
+    modify seen_type enum ('wild', 'encounter', 'nearby_stop', 'nearby_cell', 'lure_wild', 'lure_encounter') null;
