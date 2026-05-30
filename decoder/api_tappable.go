58e8613fd7af740596ce49bce1078316e68aa169
James Berry <james@coppermoth.com>
2026-05-30 21:46:29 +0100

    refactor: ApiTappableResult pointer-based + doc tags

--- decoder/api_tappable.go
+++ decoder/api_tappable.go
@@ -1,20 +1,21 @@
 package decoder
 
-import "github.com/guregu/null/v6"
-
+// ApiTappableResult is the API representation of a tappable. Nullable database
+// columns are represented as pointers (nil => JSON null) without omitempty so
+// every key is always present.
 type ApiTappableResult struct {
-	Id                      uint64      `json:"id"`
-	Lat                     float64     `json:"lat"`
-	Lon                     float64     `json:"lon"`
-	FortId                  null.String `json:"fort_id"`
-	SpawnId                 null.Int    `json:"spawn_id"`
-	Type                    string      `json:"type"`
-	Encounter               null.Int    `json:"pokemon_id"`
-	ItemId                  null.Int    `json:"item_id"`
-	Count                   null.Int    `json:"count"`
-	ExpireTimestamp         null.Int    `json:"expire_timestamp"`
-	ExpireTimestampVerified bool        `json:"expire_timestamp_verified"`
-	Updated                 int64       `json:"updated"`
+	Id                      uint64  `json:"id" doc:"Tappable encounter ID"`
+	Lat                     float64 `json:"lat" doc:"Latitude of the tappable"`
+	Lon                     float64 `json:"lon" doc:"Longitude of the tappable"`
+	FortId                  *string `json:"fort_id" doc:"ID of the fort the tappable belongs to"`
+	SpawnId                 *int64  `json:"spawn_id" doc:"ID of the spawnpoint the tappable belongs to"`
+	Type                    string  `json:"type" doc:"Type of the tappable"`
+	Encounter               *int64  `json:"pokemon_id" doc:"Pokedex ID of the encountered pokemon"`
+	ItemId                  *int64  `json:"item_id" doc:"ID of the item reward"`
+	Count                   *int64  `json:"count" doc:"Count of the item reward"`
+	ExpireTimestamp         *int64  `json:"expire_timestamp" doc:"Unix timestamp when the tappable expires"`
+	ExpireTimestampVerified bool    `json:"expire_timestamp_verified" doc:"Whether the expire timestamp is verified"`
+	Updated                 int64   `json:"updated" doc:"Unix timestamp when the record was last updated"`
 }
 
 func buildTappableResult(tappable *Tappable) ApiTappableResult {
@@ -22,13 +23,13 @@ func buildTappableResult(tappable *Tappable) ApiTappableResult {
 		Id:                      tappable.Id,
 		Lat:                     tappable.Lat,
 		Lon:                     tappable.Lon,
-		FortId:                  tappable.FortId,
-		SpawnId:                 tappable.SpawnId,
+		FortId:                  tappable.FortId.Ptr(),
+		SpawnId:                 tappable.SpawnId.Ptr(),
 		Type:                    tappable.Type,
-		Encounter:               tappable.Encounter,
-		ItemId:                  tappable.ItemId,
-		Count:                   tappable.Count,
-		ExpireTimestamp:         tappable.ExpireTimestamp,
+		Encounter:               tappable.Encounter.Ptr(),
+		ItemId:                  tappable.ItemId.Ptr(),
+		Count:                   tappable.Count.Ptr(),
+		ExpireTimestamp:         tappable.ExpireTimestamp.Ptr(),
 		ExpireTimestampVerified: tappable.ExpireTimestampVerified,
 		Updated:                 tappable.Updated,
 	}
